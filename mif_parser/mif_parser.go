bbf148c4c3e54db519549cbef9b4232d3e120d46
Paul Quintin <paulquintin@MacBook-Pro-de-Paul.local>
2022-01-31 17:10:45 +0100

    ecriture du mif (sans entete) line + op code + param

--- mif_parser/mif_parser.go
+++ mif_parser/mif_parser.go
@@ -1,4 +1,4 @@
-package mif_parser 
+package mif_parser
 
 import (
 	"bytes"
@@ -9,26 +9,27 @@ import (
 
 var label *regexp.Regexp = regexp.MustCompile("([a-z]{3,}_[a-z0-9]+)[ \t]+DCB[ \t]+0x([0-9A-F]+)")
 
+var paramreg string = "#((?:0x[0-9A-F]+)|[a-z]{3,}_[a-z0-9]+)$"
 var opcode map[string]string = map[string]string{
-	"MOV R1, ":       "01",
-	"MOV R0, ":       "02",
-	"MOV R3, ":       "03",
-	"LDRB R0, [R1]":  "04",
-	"LDRB R3, [R1]":  "05",
-	"STRB  R0, [R1]": "06",
-	"CMP  R0, R3":    "07",
-	"BEQ ":           "08",
-	"ADD R1, R1, R0": "09",
-	"BCC ":           "0A",
-	"B ":             "0B",
-	"LDRB R3, [R3]":  "0C",
-	"ADD R0, R0, ":   "0D",
-	"SUB R0,R0, R3":  "0E",
-	"AND R0,R0,R3":   "0F",
-	"LSR R0, R0, R3": "10",
-	"ADD R0, R0,R3":  "11",
-	"ADD R0, R0,R0":  "12",
-	"MOV R3, R0":     "13",
+	"MOV R1, #param":     "01",
+	"MOV R0, #param":     "02",
+	"MOV R3, #param":     "03",
+	"LDRB R0, [R1]":      "04",
+	"LDRB R3, [R1]":      "05",
+	"STRB R0, [R1]":      "06",
+	"CMP  R0, R3":        "07",
+	"BEQ #param":         "08",
+	"ADD R1, R1, R0":     "09",
+	"BCC #param":         "0A",
+	"B #param":           "0B",
+	"LDRB R3, [R3]":      "0C",
+	"ADD R0, R0, #param": "0D",
+	"SUB R0, R0, R3":     "0E",
+	"AND R0, R0, R3":     "0F",
+	"LSR R0, R0, R3":     "10",
+	"ADD R0, R0, R3":     "11",
+	"ADD R0, R0, R0":     "12",
+	"MOV R3, R0":         "13",
 }
 
 func check(e error) {
@@ -54,10 +55,76 @@ func CompileToMif(asmContent string) bytes.Buffer {
 	var b bytes.Buffer
 	content := asmContent
 	lines := strings.Split(content, "\n")
+	varLine := map[string]string{}
 	i := 0
+	for _, line := range lines {
+		for s, _ := range opcode {
+			s = strings.ReplaceAll(s, "[", `\[`)
+			s = strings.ReplaceAll(s, "]", `\]`)
+			s = strings.ReplaceAll(s, "#param", paramreg)
+
+			reg := regexp.MustCompile(s)
+
+			if reg.MatchString(line) {
+				i++
+			}
+		}
+		if g := label.FindStringSubmatch(line); !isEmpty(line) && len(g) > 0 {
+			varLine[g[1]] = fmt.Sprintf("%4X", i)
+			varLine[g[1]] = strings.ReplaceAll(varLine[g[1]], " ", "0")
+			i++
+			fmt.Println("clé :", g[1], "valeur :", varLine[g[1]])
+		}
+
+	}
+	i = 0
 	for _, line := range lines {
 
 		line = strings.Trim(line, " ")
+
+		//case of operation
+
+		for s, op := range opcode {
+			s = strings.ReplaceAll(s, "[", `\[`)
+			s = strings.ReplaceAll(s, "]", `\]`)
+			s = strings.ReplaceAll(s, "#param", paramreg)
+
+			reg := regexp.MustCompile(s)
+
+			if reg.MatchString(line) {
+				g := reg.FindStringSubmatch(line)
+				if len(g) > 1 {
+					fmt.Println(g[1])
+					//numerotation ligne + opcode
+					data := fmt.Sprintf("%2X", i)
+					data = strings.ReplaceAll(data, " ", "0")
+					data = fmt.Sprintf("%v : %v", data, op)
+					//comentaire
+					com := fmt.Sprintf("%v %v %v", "%", line, "%")
+					//parmam
+					param := fmt.Sprintf("%v", varLine[g[1]])
+					param = strings.ReplaceAll(param, " ", "0")
+					//ligne + opcode + param + commentaire
+					data = fmt.Sprintf("%v%v;\t%v\n", data, param, com)
+					write(&b, "%v", data)
+					i++
+				} else {
+					// g := reg.FindStringSubmatch(line)
+					//numerotation ligne + opcode
+					data := fmt.Sprintf("%2X", i)
+					data = strings.ReplaceAll(data, " ", "0")
+					data = fmt.Sprintf("%v : %v", data, op)
+					//comentaire
+					com := fmt.Sprintf("%v %v %v", "%", line, "%")
+					//parmam
+					param := "0000"
+					//ligne + opcode + param + commentaire
+					data = fmt.Sprintf("%v%v;\t%v\n", data, param, com)
+					write(&b, "%v", data)
+					i++
+				}
+			}
+		}
 
 		//case of variable
 		if g := label.FindStringSubmatch(line); !isEmpty(line) && len(g) > 0 {
@@ -71,7 +138,7 @@ func CompileToMif(asmContent string) bytes.Buffer {
 			//comentaire
 			com := fmt.Sprintf("%v %v %v", "%", line, "%")
 			//ligne + value + commentaire
-			data = fmt.Sprintf("%v%v\t%v\n", data, value, com)
+			data = fmt.Sprintf("%v%v;\t%v\n", data, value, com)
 
 			write(&b, "%v", data)
 			i++
@@ -79,5 +146,5 @@ func CompileToMif(asmContent string) bytes.Buffer {
 
 	}
 
-	return b 
+	return b
 }
